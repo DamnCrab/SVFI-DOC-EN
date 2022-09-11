ecd8b89fdf34423f31da2f001274ec44c3ddce54
Justin Southern Isles <2410377391@qq.com>
2022-09-11 13:38:37 +0800

    init English i18n at several key docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,258 +1,192 @@
-const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+const baiduCode = require('./config/baiduCode.js'); // Baidu statistics hm code
 const htmlModules = require('./config/htmlModules.js');
 
 
 module.exports = {
 
-	theme: 'vdoing', // 使用依赖包主题
-	// theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
+  theme: 'vdoing', // use dependency package theme
+  // theme: require.resolve('../../vdoing'), // Use a local theme (download the vdoing theme file to the local first: https://github.com/xugaoyi/vuepress-theme-vdoing)
 
-	title: "SVFI Doc",
-	description: 'SVFI用户使用文档',
-	base: '', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
-	head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-		['link', {
-			rel: 'icon',
-			href: '/img/svfi.ico'
-		}], //favicons，资源放在public文件夹
-		['meta', {
-			name: 'keywords',
-			content: 'vuepress,theme,blog,vdoing'
-		}],
-		['meta', {
-			name: 'theme-color',
-			content: '#11a8cd'
-		}], // 移动浏览器主题颜色
+  title: "SVFI Doc",
+  description: 'SVFI English Documentation',
+  base: '', // Default '/'. If you want to deploy your website to e.g. https://foo.github.io/bar/ then base should be set to "/bar/", (otherwise the page will lose styles etc.)
+  head: [ // The tag injected into the page <head>, format [tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: '/img/svfi.ico' }], //favicons, resources are placed in the public folder
+    ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // Mobile browser theme color
 
-		['meta', {
-			name: 'wwads-cn-verify',
-			content: '6c4b761a28b734fe93831e3fb400ce87'
-		}], // 广告相关，你可以去掉
-		['script', {
-			src: 'https://cdn.wwads.cn/js/makemoney.js',
-			type: 'text/javascript'
-		}], // 广告相关，你可以去掉
+    ['meta', { name: 'wwads-cn-verify', content: '6c4b761a28b734fe93831e3fb400ce87' }], // Advertising related, you can remove it
+    ['script', { src: 'https://cdn.wwads.cn/js/makemoney.js', type: 'text/javascript' }], // Advertising related, you can remove it
 
-		['meta', {
-			name: 'referrer',
-			content: 'no-referrer'
-		}],
+    ['meta', { name: 'referrer', content: 'no-referrer' }],
 
-	],
+  ],
 
-	// 主题配置
-	themeConfig: {
-		nav: [{
-				text: '首页',
-				link: '/'
-			},
-			{
-				text: '快速入门',
-				link: '/pages/dc46b8',
-				items: [{
-						text: '什么是补帧',
-						link: '/pages/88ed7f/'
-					},
-					{
-						text: '什么是超分',
-						link: '/pages/681961/'
-					},
-					{
-						text: '快速上手SVFI',
-						link: '/pages/0e988c/'
-					},
-					{
-						text: '相关文章（科普）',
-						link: '/pages/8cc1b5',
-						items: [{
-								text: '什么是编码、编码器与封装格式',
-								link: '/pages/76d9d4/'
-							},
-							{
-								text: '什么是压制',
-								link: '/pages/7b7d11/'
-							},
-							{
-								text: '什么是HDR',
-								link: '/pages/89244b/'
-							},
-						]
-					}
-				]
-			},
-			{
-				text: '指南',
-				link: '/pages/f8b952/',
-				items: [{
-						text: '高级内容详解',
-						link: '/pages/052617/'
-					},
-					{
-						text: '命令行进阶',
-						link: '/pages/ceb849/'
-					},
-				]
-			},
-			{
-				text: '常见问题解答',
-				link: '/pages/9cc27d/',
-				items: [{
-					text: '使用技巧与预设',
-					link: '/pages/18309a/'
-				}, ]
-			},
-			{
-				text: '支持',
-				link: '/pages/1b12ed/'
-			},
-		],
-		sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-		logo: '/img/svfi.ico', // 导航栏logo
-		repo: 'Justin62628/Squirrel-RIFE', // 导航栏右侧生成Github链接
-		searchMaxSuggestions: 10, // 搜索结果显示最大数
-		lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+  // theme configuration
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Quickstart', link: '/pages/dc46b8', items: [
+          { text: 'What is VFI', link: '/pages/88ed7f/' },
+          { text: 'What is SR', link: '/pages/681961/' },
+          { text: 'Quick Start for SVFI', link: '/pages/0e988c/' },
+          {
+            text: 'Related articles', link: '/pages/8cc1b5', items: [
+              { text: 'What is encoding, encoder and pixel format', link: '/pages/76d9d4/' },
+              { text: 'What is rendering', link: '/pages/7b7d11/' },
+              { text: 'What is HDR', link: '/pages/89244b/' },
+            ]
+          }
+        ]
+      },
+      {
+        text: 'Guide', link: '/pages/f8b952/', items: [
+          { text: 'Advanced Settings Explained', link: '/pages/052617/'},
+          { text: 'Using Commandine tools of SVFI', link: '/pages/ceb849/'},
+        ]
+      },
+      { text: 'FAQ', link: '/pages/9cc27d/', items: [
+        { text: 'Tips and Presets', link: '/pages/18309a/'},
+      ] },
+      { text: 'support', link: '/pages/1b12ed/' },
+    ],
+    sidebarDepth: 2, // sidebar display depth, default 1, maximum 2 (displayed to h3 title)
+    logo: '/img/svfi.ico', // navigation bar logo
+    repo: 'Justin62628/Squirrel-RIFE', // Generate a Github link on the right side of the navigation bar
+    searchMaxSuggestions: 10, // maximum number of search results displayed
+    lastUpdated: 'Last updated', // Update time, and prefix text string | boolean (value is git commit time)
 
-		// docsDir: 'docs', // 编辑的文件夹
-		// editLinks: true, // 编辑链接
-		// editLinkText: '编辑',
+    // docsDir: 'docs', // edited folder
+    // editLinks: true, // edit links
+    // editLinkText: 'Edit',
 
-		// 以下配置是Vdoing主题改动的和新增的配置
-		sidebar: {
-			mode: 'structuring',
-			collapsable: false
-		}, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    // The following configuration is the changed and new configuration of Vdoing theme
+    sidebar: { mode: 'structuring', collapsable: false }, // sidebar 'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | Customization Tips: Contents page data depends on structure Customized sidebar data, if you don't set it to 'structuring', you will not be able to use the table of contents page
 
-		// sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
-		updateBar: { // 最近更新栏
-			showToArticle: false, // 显示到文章页底部，默认true
-			// moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
-		},
-		// titleBadge: false, // 文章标题前的图标是否显示，默认true
-		// titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
-		//   '图标地址1',
-		//   '图标地址2'
-		// ],
+    // sidebarOpen: false, // Whether to open the sidebar in the initial state, the default is true
+    updateBar: { // Recently updated bar
+      showToArticle: false, // show to the bottom of the article page, default true
+      // moreArticle: '/archives' // "More Articles" jump page, default '/archives'
+    },
+    // titleBadge: false, // Whether the icon before the title of the article is displayed, the default is true
+    // titleBadgeIcons: [ // The address of the icon before the title of the article, the default theme built-in icon
+    // 'Icon address 1',
+    // 'Icon address 2'
+    // ],
 
-		pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+    pageStyle: 'line', // page style, optional value: 'card' card | 'line' line (only valid when bodyBgImg is not set), default 'card'. Description: When the card is displayed, the background is gray to set off the card style. When the line is used, the background is displayed in solid color, and some modules have line borders.
 
-		// contentBgStyle: 1,
+    // contentBgStyle: 1,
 
-		category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-		tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
-		// archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+    category: false, // Whether to open the category function, the default is true. If it is opened, it will do the following: 1. The automatically generated frontmatter contains the category field 2. The page displays information and modules related to the category 3. The category page (in the @pages folder) is automatically generated. If closed, vice versa.
+    tag: false, // Whether to open the tag function, the default is true. If it is opened, it will do the following: 1. The automatically generated frontmatter contains the tag field 2. The page displays information and modules related to the tag 3. The tag page is automatically generated (in the @pages folder). If closed, vice versa.
+    // archive: false, // Whether to open the archive function, the default is true. If open, it will do the following: 1. Automatically generate archived pages (in the @pages folder). If closed, vice versa.
 
-		author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-			name: 'Justin62628', // 必需
-			href: 'https://github.com/Justin62628' // 可选的
-		},
-		social: { // 社交图标，显示于博主信息栏和页脚栏
-			// iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-			icons: [{
-					iconClass: 'icon-youjian',
-					title: '发邮件',
-					link: '2410377391@qq.com'
-				},
-				{
-					iconClass: 'icon-github',
-					title: 'GitHub',
-					link: 'https://github.com/Justin62628/Squirrel-RIFE'
-				},
-			]
-		},
-		footer: { // 页脚信息
-			createYear: 2022, // 博客创建年份
-			copyrightInfo: 'Justin62628 | MIT License', // 博客版权信息，支持a标签
-		},
-		htmlModules,
-	},
+    author: { // The default author information of the article, which can be configured separately in the md file String | {name: String, href: String}
+      name: 'Justin62628', // required
+      href: 'https://github.com/Justin62628' // optional
+    },
+    social: { // social icon, displayed in blogger info bar and footer bar
+      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // optional, the online css file address of Ali icon library, you can freely add icons that are not in the theme
+      icons: [
+        {
+          iconClass: 'icon-youjian',
+          title: 'Send Email',
+          link: 'https://github.com/Justin62628'
+        },
+        {
+          iconClass: 'icon-github',
+          title: 'GitHub',
+          link: 'https://github.com/Justin62628/Squirrel-RIFE'
+        },
+      ]
+    },
+    footer: { // footer information
+      createYear: 2022, // Blog creation year
+      copyrightInfo: 'Justin62628 | MIT License', // Blog copyright information, support a tag
+    },
+    htmlModules,
+  },
 
-	// 插件
-	plugins: [
-		// [require('./plugins/love-me'), { // 鼠标点击爱心特效
-		//   color: '#11a8cd', // 爱心颜色，默认随机色
-		//   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
-		// }],
-		[
-			'vuepress-plugin-mathjax',
-			{
-				target: 'svg',
-				macros: {
-					'*': '\\times',
-				},
-			},
-		],
-		['fulltext-search'], // 全文搜索
+  // plugin
+  plugins: [
+    // [require('./plugins/love-me'), { // Mouse click love effect
+    // color: '#11a8cd', // heart color, default random color
+    // excludeClassName: 'theme-vdoing-content' // class of the element to be excluded, default empty''
+    // }],
 
-		// ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-		//   thirdparty: [ // 可选，默认 []
-		//     {
-		//       title: '在GitHub中搜索',
-		//       frontUrl: 'https://github.com/search?q=', // 搜索链接的前面部分
-		//       behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
-		//     },
-		//     {
-		//       title: '在npm中搜索',
-		//       frontUrl: 'https://www.npmjs.com/search?q=',
-		//     },
-		//     {
-		//       title: '在Bing中搜索',
-		//       frontUrl: 'https://cn.bing.com/search?q='
-		//     }
-		//   ]
-		// }],
+    ['fulltext-search'], // full text search
 
-		[
-			'vuepress-plugin-baidu-tongji', // 百度统计
-			{
-				// hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
-			}
-		],
+    // ['thirdparty-search', { // A search box where third-party search links can be added (the parameters of the original official search box are still available)
+    // thirdparty: [ // optional, default []
+    // {
+    // title: 'Search in GitHub',
+    // frontUrl: 'https://github.com/search?q=', // search the front part of the link
+    // behindUrl: '' // search the back part of the link, optional, default ''
+    // },
+    // {
+    // title: 'Search in npm',
+    // frontUrl: 'https://www.npmjs.com/search?q=',
+    // },
+    // {
+    // title: 'Search in Bing',
+    // frontUrl: 'https://cn.bing.com/search?q='
+    // }
+    // ]
+    // }],
 
-		['one-click-copy', { // 代码块复制按钮
-			copySelector: ['div[class*="language-"] pre',
-			'div[class*="aside-code"] aside'], // String or Array
-			copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-			duration: 1000, // prompt message display time.
-			showInMobile: false // whether to display on the mobile side, default: false.
-		}],
-		['demo-block', { // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-			settings: {
-				// jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-				// cssLib: ['http://xxx'], // 在线示例中的css依赖
-				// vue: 'https://fastly.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-				jsfiddle: false, // 是否显示 jsfiddle 链接
-				codepen: true, // 是否显示 codepen 链接
-				horizontal: false // 是否展示为横向样式
-			}
-		}],
-		[
-			'vuepress-plugin-zooming', // 放大图片
-			{
-				selector: '.theme-vdoing-content img:not(.no-zoom)',
-				options: {
-					bgColor: 'rgba(0,0,0,0.6)'
-				},
-			},
-		],
-		[
-			'@vuepress/last-updated', // "上次更新"时间格式
-			{
-				transformer: (timestamp, lang) => {
-					const dayjs = require('dayjs') // https://day.js.org/
-					return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
-				},
-			}
-		]
-	],
+    [
+      'vuepress-plugin-baidu-tongji', // Baidu statistics
+      {
+        // hm: baiduCode || '01293bffa6c3962016c08ba685c79d78'
+      }
+    ],
 
-	markdown: {
-		// lineNumbers: true,
-		extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
-	},
+    ['one-click-copy', { // code block copy button
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: 'Copy successfully', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+    ['demo-block', { // demo demo module https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      settings: {
+        // jsLib: ['http://xxx'], // js dependencies in online examples (jsfiddle, codepen)
+        // cssLib: ['http://xxx'], // css dependencies in online example
+        // vue: 'https://fastly.jsdelivr.net/npm/vue/dist/vue.min.js', // vue dependencies in the online example
+        jsfiddle: false, // whether to show jsfiddle link
+        codepen: true, // whether to show the codepen link
+        horizontal: false // whether to display as a horizontal style
+      }
+    }],
+    [
+      'vuepress-plugin-zooming', // zoom in on the image
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)'
+        },
+      },
+    ],
+    [
+      '@vuepress/last-updated', // "last-updated" time format
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ]
+  ],
 
-	// 监听文件变化并重新构建
-	extraWatchFiles: [
-		'.vuepress/config.js',
-		'.vuepress/config/htmlModules.js',
-	]
+  markdown: {
+    // lineNumbers: true,
+    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // Extract headers to the sidebar level, default ['h2', 'h3']
+  },
+
+  // listen for file changes and rebuild
+  extraWatchFiles: [
+    '.vuepress/config.js',
+    '.vuepress/config/htmlModules.js',
+  ]
 }
--- docs/index.md
+++ docs/index.md
@@ -2,24 +2,24 @@
 home: true
 heroImage: /img/logo.webp
 heroText: Squirrel-RIFE Video Frame Interpolation
-tagline: 
-actionText: 专注于动漫超分补帧 →
+tagline:
+actionText: Designed for Anime Enhance →
 actionLink: pages/0e988c/
-bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+bannerBg: none # auto => grid background (no background when there is bodyBgImg), default | none => none | 'big picture address' | background: custom background style Tips: if you find that the text color does not fit your background You can go to palette.styl to modify the $bannerTextColor variable
 
-features: 
-  - title: 上手即用
-    details: 自动预设系统，小白也能用，根据输入自动调整设置，获得最优效果
-  - title: 高质量输出
-    details: 高粒度设置选项，满足你的极客心与质量把控高要求
-  - title: 快速省心
-    details: 超多优化避免常见补帧软件造成的果冻，转场花屏等不如意
+features:
+  - title: Out of the Box
+    details: Automatic preset system, automatically adjust settings to obtain the best outcome
+  - title: High Quality
+    details: Settings with high granularity and tons of options for high quality control 
+  - title: Fast Process
+    details: Lots of optimizations to avoid jelly scenes caused by common vfi software, blurred transitions, etc.
 
-# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+# Article list display mode: detailed By default, display the detailed version of the article list (including author, category, tag, abstract, pagination, etc.) | simple => Display the simplified version of the article list (only title and date) | none Do not display the article list
 postList: none
 ---
 <p align="center">
-  <a class="become-sponsor" href="https://store.steampowered.com/app/1692080/SVFI/">STEAM购买</a>
+  <a class="become-sponsor" href="https://store.steampowered.com/app/1692080/SVFI/">On STEAM</a>
 </p>
 
 <style>
@@ -35,13 +35,13 @@ postList: none
 
 <br/>
 <p align="center">
-  <!-- <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/v/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a> -->
-  <!-- <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/dt/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a> -->
+  <!-- <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/ v/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a> -->
+  <!-- <a href="https://www.npmjs.com/package/vuepress-theme-vdoing" target="_blank"><img src="https://img.shields.io/npm/ dt/vuepress-theme-vdoing" alt="npm" class="no-zoom"></a> -->
   <a href="https://github.com/Justin62628/Squirrel-RIFE" target="_blank"><img src='https://img.shields.io/github/stars/Justin62628/Squirrel-RIFE' alt='GitHub stars' class="no-zoom"></a>
   <a href="https://github.com/Justin62628/Squirrel-RIFE" target="_blank"><img src='https://img.shields.io/github/forks/Justin62628/Squirrel-RIFE' alt='GitHub forks' class="no-zoom"></a>
 </p>
 
-## 🌎 效果演示 Demonstration on BiliBili
+## 🌎 Demonstrations on BiliBili
 
 ::: cardImgList
 ```yaml
@@ -53,36 +53,36 @@ config:
 
 data:
 - img: /img/bilibili/yuan.jpg
-  name: 《原神》
-  desc: 剧情CM短片 8K60帧 SVFI补帧超分
+  name: Genshin Impact
+  desc: Drama CM short film, 8K 60fps
   link: https://www.bilibili.com/video/BV1FS4y1C7RD
-  author: SVFI视觉 
+  author: SVFI Vision
   avatar: /img/svfi.ico
 - img: /img/bilibili/umaron.jpg
-  name: 赛马娘
-  desc:  第二季 NCOP 8K60帧
+  name: Umaron
+  desc: Season 2 NCOP 8K 60fps
   link: https://www.bilibili.com/video/BV1QY411b7e4
-  author: SVFI视觉 
+  author: SVFI Vision
   avatar: /img/svfi.ico
 - img: /img/bilibili/emilia.jpg
-  name:  Re：从零开始的异世界生活
-  desc: 第二季后半 NCED Believe in you
+  name: Re Zero-Starting Life in Another World
+  desc: Season 2 NCED Believe in you
   link: https://www.bilibili.com/video/BV1kF411p7FB
-  author: SVFI视觉 
+  author: SVFI Vision
   avatar: /img/svfi.ico
 ```
 :::
 
-## 🎖 白嫖列表
+## 🎖 Free Apps!
 ::: cardList 2
 ```yaml
 - name: Squirrel Anime Enhance
-  desc: 🚀开源超分软件，专注于兼容的超分工具
+  desc: 🚀Open source super-resolution application
   link: https://github.com/Justin62628/Squirrel-RIFE/releases/tag/v3.20.4
   bgColor: '#eaeef1'
   textColor: '#2A3344'
-- name: SVFI Demo 预览版
-  desc: SVFI的预览版，支持1000帧以内的高质量、高自定义的补帧和超分
+- name: SVFI Demo
+  desc: The demo version of SVFI, which supports high-quality, highly customized VFI and SR output within 1000 frames
   link: https://store.steampowered.com/search/?sort_by=_ASC&term=SVFI+Demo
   bgColor: '#eaeef1'
   textColor: '#2A3344'
@@ -91,36 +91,29 @@ data:
 
 <br/>
 
-## 💻 系统要求
+## 💻 System Requirements
 
-  -  Windows 10 及以上操作系统
+  - Windows 10 or above
 
-  -  Windows 10 or above
+  - NVIDIA GPU (> GTX 750ti) or AMD GPU released within 5 years
 
-  -  NVIDIA GTX 750ti 以上的N卡或五年内发布的A卡
-
-  -  NVIDIA GPU( > GTX 750ti) or AMD GPU released within 5 years
-
-  -  2GB 以上显存, 4GB 以上的空余运行内存以及10GB+的磁盘剩余空间
-
-  -  2GB+ of VRAM, 4GB+ of RAM and 10GB+ of free disk space
+  - 2GB+ of VRAM, 4GB+ of RAM and 10GB+ of free disk space
 
 
-## ⚡ 反馈与交流
+## ⚡ Feedback & Communication
 
-在软件使用过程中有任何问题和想法，请在 [Steam论坛](https://steamcommunity.com/app/1692080/discussions/1/)提问。
-或者加入[BBS论坛](https://bbs.svfi.group/)：
+If you have any questions or advice using of the software, please feel free to post in the [Steam Forum](https://steamcommunity.com/app/1692080/discussions/1/).
+Or join the [BBS Forum](https://bbs.svfi.group/):
 
-## 🤝  参考 Reference & Acknowledgement
+## 🤝 Reference & Acknowledgement
 - Video Frame Interpolation Algorithm: [RIFE](https://github.com/hzwer/arXiv2020-RIFE)
-- NCNN Support: [RIFE-NCNN](https://github.com/nihui/rife-ncnn-vulkan) 
+- NCNN Support: [RIFE-NCNN](https://github.com/nihui/rife-ncnn-vulkan)
 - SWIG Wraps: [Everything-ncnn-vulkan-python](https://github.com/orgs/media2x/repositories)
 - UI Design: [QCandyUi](https://github.com/shuoGG1239/QCandyUi)
 - Steamworks Interface: [SteamworksPy](https://github.com/philippj/SteamworksPy)
-- Encode Supports: [FFmpeg](https://github.com/FFmpeg/FFmpeg), [QSVEnc](https://github.com/rigaya/QSVEnc), [NVEnc](https://github.com/rigaya/NVEnc), [dovi_tool](https://github.com/quietvoid/dovi_tool), [dlb_mp4base](https://github.com/DolbyLaboratories/dlb_mp4base), [hdr10plus_parser](https://github.com/quietvoid/hdr10plus_parser)
-- Super Resolution Algorithm: [waifu2x](https://github.com/nagadomi/waifu2x), [RealESR](https://github.com/xinntao/Real-ESRGAN), [RealCUGAN](https://github.com/bilibili/ailab/blob/main/Real-CUGAN/LICENSE)
+- Encode Supports: [FFmpeg](https://github.com/FFmpeg/FFmpeg), [QSVEnc](https://github.com/rigaya/QSVEnc), [NVEnc](https://github.com/ rigaya/NVEnc), [dovi_tool](https://github.com/quietvoid/dovi_tool), [dlb_mp4base](https://github.com/DolbyLaboratories/dlb_mp4base), [hdr10plus_parser](https://github.com /quietvoid/hdr10plus_parser)
+- Super Resolution Algorithm: [waifu2x](https://github.com/nagadomi/waifu2x), [RealESR](https://github.com/xinntao/Real-ESRGAN), [RealCUGAN](https://github .com/bilibili/ailab/blob/main/Real-CUGAN/LICENSE)
 - View More at `LICENSE` and `LICENSES_BUNDLE` FILE
 
-## 👓 一些注意事项
-- 根据Steam用户使用条款(EULA)，SVFI发行版输出内容不可用于商业用途
+## 👓 Some notes
 - According to EULA, SVFI's output is not allowed for commercial use.
